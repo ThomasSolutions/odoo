b68dabf6f1577bb72459d70c1914cf36495f861c
andrewbates <andrew@kadabby.com>
2018-12-16 20:21:18 -0500

    updates to security model

--- thomasfleet/__manifest__.py
+++ thomasfleet/__manifest__.py
@@ -15,7 +15,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Human Resources',
+    'category': 'Thomas Solutions',
     'version': '0.38',
 
     # any module necessary for this one to work correctly
