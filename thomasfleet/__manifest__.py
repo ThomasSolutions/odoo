a5a20cb24106f1d5922fcfcce97e9f37a1bbfcd5
andrewbates <andrew@kadabby.com>
2019-09-20 09:27:37 -0400

    Updates to securitry model for customer_department

--- thomasfleet/__manifest__.py
+++ thomasfleet/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Thomas',
-    'version': '0.62',
+    'version': '0.64',
 
     # any module necessary for this one to work correctly
     'depends': ['base','fleet'],
