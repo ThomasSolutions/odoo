e2314c774bf1a7c46a7433215fcdc0c4e5c515f1
andrewbates <andrew@kadabby.com>
2019-11-21 12:25:19 -0500

    added sale module dependency back

--- thomasfleet/__manifest__.py
+++ thomasfleet/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.91',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','fleet', 'account'],
+    'depends': ['base','fleet', 'account' , 'sale'],
 
     # always loaded
     'data': [
