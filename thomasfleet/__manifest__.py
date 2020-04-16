49c3a858df5bc29bf00e083313026d67d5504fbb
andrewbates <andrew@kadabby.com>
2020-04-16 12:17:53 -0400

    version bump

--- thomasfleet/__manifest__.py
+++ thomasfleet/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Thomas',
-    'version': '0.176',
+    'version': '0.177',
 
 
 
