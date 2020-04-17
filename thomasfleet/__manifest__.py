61155fb5bdbb9baed0e3c6e034c515118656da20
andrewbates <andrew@kadabby.com>
2020-04-17 12:15:43 -0400

    update lease print out

--- thomasfleet/__manifest__.py
+++ thomasfleet/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Thomas',
-    'version': '0.177',
+    'version': '0.178',
 
 
 
