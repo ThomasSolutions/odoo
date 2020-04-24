0de1c5d01710de49e07fa613c846249e4ed593c5
andrewbates <andrew@kadabby.com>
2020-04-24 09:41:07 -0400

    updates

--- thomasfleet/__manifest__.py
+++ thomasfleet/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Thomas',
-    'version': '0.184',
+    'version': '0.185',
 
 
 
