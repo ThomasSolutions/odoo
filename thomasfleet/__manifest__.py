218c066bf8e5e89da8139e1128a67d4909ced191
andrewbates <andrew@kadabby.com>
2020-09-02 09:19:27 -0400

    security update to menu items

--- thomasfleet/__manifest__.py
+++ thomasfleet/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Thomas',
-    'version': '0.220',
+    'version': '0.221',
 
 
     # any module necessary for this one to work correctly
