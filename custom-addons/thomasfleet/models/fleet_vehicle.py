35a10069e82deb57e8f4474807ad0271a514150b
realariful <real_ariful@hotmail.com>
2022-10-25 16:23:23 +0600

    UPGRADES

--- custom-addons/thomasfleet/models/fleet_vehicle.py
+++ custom-addons/thomasfleet/models/fleet_vehicle.py
@@ -22,28 +22,28 @@ MODEL_FIELDS_TO_VEHICLE = {
 }
 
 
-# class ThomasFleetTest(models.Model):
-#     _inherit = 'fleet.vehicle'
+class ThomasFleetTest(models.Model):
+    _inherit = 'fleet.vehicle'
 
-    # unit_int = fields.Integer(compute='_getInteger', store=True)
+    unit_int = fields.Integer(compute='_getInteger', store=True)
 
-#     @api.depends('unit_no')
-#     def _getInteger(self):
-#         for rec in self:
-#             try:
-#                 rec.unit_int = int(rec.unit_no)
-#             except ValueError:
-#                 rec.unit_int = 0
-#                 raise models.ValidationError('Protractor Unit # ' + rec.unit_no
-#                                              + ' is not valid (it must be an integer)')
+    @api.depends('unit_no')
+    def _getInteger(self):
+        for rec in self:
+            try:
+                rec.unit_int = int(rec.unit_no)
+            except ValueError:
+                rec.unit_int = 0
+                raise models.ValidationError('Protractor Unit # ' + rec.unit_no
+                                             + ' is not valid (it must be an integer)')
 
-    # def default_unit_no(self):
-    #     last_vehicle = self.env['fleet.vehicle'].search([], limit=1, order='unit_int desc')
-    #     print('Last Unit #' + str(last_vehicle.unit_no))
-    #     return str(int(last_vehicle.unit_no) + 1)
+    def default_unit_no(self):
+        last_vehicle = self.env['fleet.vehicle'].search([], limit=1, order='unit_int desc')
+        print('Last Unit #' + str(last_vehicle.unit_no))
+        return str(int(last_vehicle.unit_no) + 1)
 
 
-    # unit_no = fields.Char("Unit #", default=default_unit_no, required=True, tracking=True)
+    unit_no = fields.Char("Unit #", default=default_unit_no, required=True, tracking=True)
 
 
 class ThomasFleetVehicle(models.Model):
@@ -54,19 +54,18 @@ class ThomasFleetVehicle(models.Model):
 
     log = logging.getLogger('thomas')
     log.setLevel(logging.INFO)
-    # unit_int = fields.Integer(compute='_getInteger', store=True)
-    unit_int = fields.Integer(store=True)
+    unit_int = fields.Integer(compute='_getInteger', store=True)
     
-    # @api.depends('unit_no')
-    # def _getInteger(self):
-    #     for rec in self:
-    #         try:
-    #             rec.unit_int = int(rec.unit_no)
-    #         except ValueError:
-    #             rec.unit_int = 0
-    #             raise models.ValidationError('Protractor Unit # ' + rec.unit_no
+    @api.depends('unit_no')
+    def _getInteger(self):
+        for rec in self:
+            try:
+                rec.unit_int = int(rec.unit_no)
+            except ValueError:
+                rec.unit_int = 0
+                raise models.ValidationError('Protractor Unit # ' + rec.unit_no
 
-    #                                          + ' is not valid (it must be an integer)')
+                                             + ' is not valid (it must be an integer)')
 
     
 
@@ -75,11 +74,7 @@ class ThomasFleetVehicle(models.Model):
         print('Last Unit #' + str(last_vehicle.unit_no))
         return str(int(last_vehicle.unit_no) + 1)
 
-    #unit_no = fields.Char("Unit #", default=default_unit_no, required=True, tracking=True)
-    unit_no = fields.Char("Unit #", required=True, tracking=True)
     
-    
-    ################
     cost_report = fields.Float("Cost Report", readonly=True, store=True)
 
     @api.model
@@ -113,7 +108,7 @@ class ThomasFleetVehicle(models.Model):
     # name = fields.Char(compute='_compute_vehicle_name', store=True)
 
     #plate registration?
-    # unit_no = fields.Char("Unit #", default=default_unit_no, required=True, tracking=True)
+    unit_no = fields.Char("Unit #", default=default_unit_no, required=True, tracking=True)
     protractor_workorders = fields.One2many('thomasfleet.workorder', 'vehicle_id', 'Work Orders')
     lease_agreements = fields.One2many('thomaslease.lease','vehicle_id', 'Rental Agreements')
     lease_invoice_ids = fields.Many2many('account.move',string='Invoices',
