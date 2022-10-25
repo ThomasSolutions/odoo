a4f7052ed0bf9ccc87906550dd3e0869799eb515
realariful <real_ariful@hotmail.com>
2022-10-25 17:07:20 +0600

    UPDATES

--- thomasfleet/__manifest__.py
+++ thomasfleet/__manifest__.py
@@ -6,7 +6,7 @@
     'author': "Andrew Bates[Dev], AZM Ariful Haque Real[Upgrade]",
     'website': "https://www.syncoria.com",
     'category': 'Human Resources/Fleet',
-    'version': '15.4.4',
+    'version': '15.4.5',
     'depends': ['base','web', 'fleet', 'account'],
     'data': [
         'security/thomasfleet_security.xml',
